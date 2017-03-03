85a3bc97952e50cbc44d3c23cad717e8048376c5
Adrian Guerrero <adrian@foostudio.mx>
2017-03-02 18:57:12 -0600

    Cache assets with service worker

--- web-workers/serviceWorker.js
+++ web-workers/serviceWorker.js
@@ -1,3 +1,18 @@
+var CACHE_NAME = 'service-worker-cache-v1'
+var ulrArr = [
+    'css/main.css',
+    'assets/webmvp8_512kb.mp4',
+    'assets/302H.jpg',
+    'assets/305H.jpg'
+]
+
 self.addEventListener('install', function (e) {
-    console.log(e)
+    e.waitUntil(
+        caches
+            .open(CACHE_NAME)
+            .then(function (cache) {
+                console.log('Opened cached')
+                return cache.addAll(ulrArr)
+            })
+    )
 })
